cc2cb936e3826697ad337cb9d2b9d7b1be953619
hamzabla <hamzabessa19@gmail.com>
2022-08-10 16:46:01 +0200

    crud user done

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -80,7 +80,7 @@ if (user) {
 }});
 
 
-
+//get user by id
 const getUserById = asyncHandler(async (req, res, next) => {
     const { id } = req.params;
     let user = '';
@@ -102,17 +102,118 @@ const getUserById = asyncHandler(async (req, res, next) => {
         );
     } else {
         res.status(404).send('user do not exist');
-        /*req.smartShelf = { errorCode: "user not exist" }
+        /*req.salva = { errorCode: "user not exist" }
         next();
         return;*/
     }
-
 });
 
+const getAllUsers = asyncHandler(async (req, res, next) => {
+    let users = [];
+    const querySnapshot = await db.collection('users').get();
+    querySnapshot.forEach((doc) => {
+        let added= doc.data();
+        users.push(added) ;
+});
+
+    res.status(200).json(
+        {
+            success: true,
+            operation: "getting all users ",
+            count: users.length,
+            data: {
+                users
+            }
+
+        }
+    );
+});
+
+
+//update user
+const updateUser = asyncHandler(async (req, res, next) => {
+    const { id } = req.params;
+    const {
+        fullName,
+        userName,
+        email,
+        phone,
+        password,
+        role
+    } = req.body;
+
+    if (
+        !fullName ||
+        !userName ||
+        !email ||
+        !phone ||
+        !password ||
+        !role
+    ) {
+        res.status(400).send('missing required values');
+        /*req.salva = { errorCode: "missing required values" }
+        next();
+        return;*/
+    }
+    
+    const conn= db.collection('users');
+ 
+   const user = await conn.doc(id);
+   await user.update({
+    fullName,
+    userName,
+    email,
+    phone,
+    password,
+    role
+});
+
+
+const querySnapshot = await db.collection('users').get();
+querySnapshot.forEach((doc) => {
+if(doc.id ===  id) {
+    res.status(200).json(
+        {
+            success: true,
+            operation: "updating user",
+            data: doc.data()
+
+        });
+}
+});
+   
+});
+
+// delete user
+const deleteUser = asyncHandler(async (req, res, next) => {
+    const { id } = req.params;
+    const conn= db.collection('users');
+    let userDeleted='';
+    const querySnapshot = await db.collection('users').get();
+    querySnapshot.forEach((doc) => {
+     if(doc.id ===  id) {
+    userDeleted=doc.data();
+}
+});
+    await conn.doc(id).delete();
+    res.status(200).json(
+        {
+            success: true,
+            operation: "delete user ",
+            data: {
+                userDeleted
+            }
+
+        }
+    );
+});
 
 
 
 module.exports = {
     addUser,
-    getUserById
+    getUserById,
+    getAllUsers,
+    updateUser,
+    deleteUser
 }
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const { addUser, getUserById } = require('../controllers/userController');
+const { addUser, getUserById, getAllUsers, updateUser, deleteUser  } = require('../controllers/userController');
 
-router.get("/:id", getUserById).post("/", addUser);
+router.get("/:id", getUserById).get("/",getAllUsers).post("/", addUser).put("/:id",updateUser).delete("/:id",deleteUser);
 
 //router.get("/:id", getUserById).get("/",getAllUsers).post("/",createUser).put("/:id",updateUser).delete("/:id",deleteUser);
 
