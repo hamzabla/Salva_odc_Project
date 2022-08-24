1fca13d2f853f4d254152ef84e924648bdca91ef
houda el ibrahimi <92468179+houdaibr@users.noreply.github.com>
2022-08-24 12:04:03 +0100

    initial commit

---
+++ server/controllers/sectionController.js
@@ -0,0 +1,203 @@
+console.log("hello");
+const {db} = require("../helpers/firestoreHandler");
+console.log("Hey");
+const Section = require('../models/sectionModel');
+const asyncHandler = require("express-async-handler");
+
+// add section
+
+const addSection = asyncHandler(async(req,res,next)=>{
+try{
+  let {Title,Picture,Description,Adress,Tags} = req.body;
+  console.log(req.body);
+  if (
+    ! Title||
+    ! Picture||
+    !Description ||
+    !Adress ||
+    !Tags 
+) {
+    
+req.salva = { errorCode: "missing required values" }
+    next();
+    return;
+}
+
+const conn= db.collection('sections').doc();
+
+
+
+
+ const section= await conn.set({
+    Title,
+    Picture,
+    Description,
+    Adress,
+    Tags
+});
+
+if (section) {
+    res.status(201).json({
+        success: true,
+        operation: "add new section",
+        data: {
+            Title,
+            Picture,
+            Description,
+            Adress,
+            Tags
+        }
+    });
+} else {
+    res.status(404).send("invalid section data");
+    req.salva = { errorCode: "invalid section data" }
+    next();
+    return;
+
+}
+
+
+} catch (err) {
+    req.salva = { errorCode: "invalid section data" }
+        next();
+        return;
+}
+
+});
+const getSectionById = asyncHandler(async (req, res, next) => {
+    const { id } = req.params;
+    let section = '';
+    const querySnapshot = await db.collection('sections').get();
+    querySnapshot.forEach((doc) => {
+    if(doc.id ===  id) {
+        section = doc.data();
+    }
+});
+    if (section) {
+        res.status(200).json(
+            {
+                success: true,
+                operation: "getting section by id",
+                data: section
+                
+
+            }
+        );
+    } else {
+        req.salva = { errorCode: "section not exist" }
+        next();
+        return;
+    }
+
+});
+const getAllSections = asyncHandler(async (req, res, next) => {
+    let users = [];
+    const querySnapshot = await db.collection('sections').get();
+    querySnapshot.forEach((doc) => {
+        let added= doc.data();
+        users.push(added) ;
+});
+
+    res.status(200).json(
+        {
+            success: true,
+            operation: "getting all sections ",
+            count: users.length,
+            data: {
+                users
+            }
+
+        }
+    );
+});
+const updateSection = asyncHandler(async (req, res, next) => {
+    const { id } = req.params;
+    const {
+        Title,
+        Picture,
+        Description,
+        Adress,
+        Tags,
+        
+    } = req.body;
+
+    if (
+        !Title ||
+        !Picture ||
+        !Description ||
+        !Adress ||
+        !Tags 
+    ) {
+        res.status(400).send('missing required values');
+        /*req.salva = { errorCode: "missing required values" }
+        next();
+        return;*/
+    }
+    
+    const conn= db.collection('sections');
+ 
+   const section = await conn.doc(id);
+   await section.update({
+    Title,
+    Picture,
+    Description,
+    Adress,
+    Tags,
+    
+});
+
+
+const querySnapshot = await db.collection('sections').get();
+querySnapshot.forEach((doc) => {
+if(doc.id ===  id) {
+    res.status(200).json(
+        {
+            success: true,
+            operation: "updating section",
+            data: doc.data()
+
+        });
+}
+});
+   
+});
+
+
+const deleteSection = asyncHandler(async (req, res, next) => {
+    const { id } = req.params;
+    const conn= db.collection('sections');
+    let sectionDeleted='';
+    const querySnapshot = await db.collection('sections').get();
+    querySnapshot.forEach((doc) => {
+     if(doc.id ===  id) {
+    sectionDeleted=doc.data();
+}
+});
+    await conn.doc(id).delete();
+    res.status(200).json(
+        {
+            success: true,
+            operation: "delete section ",
+            data: {
+                sectionDeleted
+            }
+
+        }
+    );
+});
+
+
+
+
+
+
+
+  
+
+module.exports = {
+    addSection,
+    getSectionById,
+    getAllSections,
+    updateSection,
+    deleteSection
+}
--- server/server.js
+++ server/server.js
@@ -1,1 +1,19 @@
-console.log("Houda");
+const express = require("express");
+const colors = require("colors");
+const morgan = require("morgan");
+const config = require("./helpers/config");
+const cors = require("cors");
+const sections = require('./routes/sectionRoute');
+
+
+
+const app = express();
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/api/v1/sections',sections);
+
+
+app.listen(config.port, () => console.log(`Server started on port ${config.port}`));
