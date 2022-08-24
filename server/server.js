80b38fa15ad3a6905f360c272ad2cf8d8d0218aa
elbachir-salik <salik.elbachir@gmail.com>
2022-08-24 11:11:40 +0100

    some modif on the server

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const colors = require("colors");
 const morgan = require("morgan");
 const config = require("./helpers/config");
 const cors = require("cors");
+const request = require('request');
 
 const reviews = require('./routes/reviewRoute');
 
@@ -14,6 +15,15 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// app.get('/home', function(req, res) {
+//     console.log('hey');
+//     request('http://127.0.0.1:3000/flask', function (error, response, body) {
+//         console.error('error:', error); // Print the error
+//         console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+//         console.log('body:', body); // Print the data received
+//         res.send(body); //Display the response on the website
+//       });      
+// });
 app.use('/api/v1/reviews',reviews);
 
 
